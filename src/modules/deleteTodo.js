1ebb7b7bcff71b57b74da5fdad11fed5a6d41624
Supreeti <sksupreetk11@gmail.com>
2023-03-11 22:27:21 +0530

    Updates

--- src/modules/deleteTodo.js
+++
@@ -1,8 +0,0 @@
-const deleteTodo = (index) => {
-  const todoIndex = document.getElementById(`todo${index}`);
-  if (todoIndex !== null) {
-    todoIndex.remove();
-  }
-};
-
-export default deleteTodo;
